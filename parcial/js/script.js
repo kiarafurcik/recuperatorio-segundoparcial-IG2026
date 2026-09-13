e2477c85a04f42ab156cbe130f1f93609072d94b
kiarafurcik <kiarafurcik@gmail.com>
2026-09-12 23:20:35 -0300

    Sistema datos empezado

--- parcial/js/script.js
+++ parcial/js/script.js
@@ -72,5 +72,5 @@ let botonTamaño = document.querySelector("#personalizar");
     }
 });
 
-// Datos al azar
+// Datos al azar con un switch, cada caso un dato. Al tocar un botón toma un número al azar que va a coincidir con un dato
 
